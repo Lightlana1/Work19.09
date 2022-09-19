92f24ce2351eb63ccf45b3671a9da83b8d04a7c0
lightlana <lebendaufdensternen@gmail.com>
2022-09-19 10:49:32 +0300

    Программа, которая на вход принимает три числа и выдаёт, какое число наибольшее

---
+++ Задача 4/Задача 4.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+if(number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if(max > number3)
+{
+    Console.WriteLine("Максимальное число " + max);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + number3);
+}
+
