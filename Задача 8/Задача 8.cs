b3c4b9074fb7aa55d97c2081ba8b00abd0b40e42
lightlana <lebendaufdensternen@gmail.com>
2022-09-19 11:42:34 +0300

    Программа, которая на вход принимает число N>0, а на выходе показывает все чётные числа от 1 до N

---
+++ Задача 8/Задача 8.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число больше 0:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+if(number1 <= 0)
+{
+    Console.WriteLine("Вы ввели неверное значение");
+}
+else
+{
+    while (number2 <= number1)
+    {
+        Console.Write(number2 + ",");
+        number2 = number2 + 2;
+    }
+}
